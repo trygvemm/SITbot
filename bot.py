23d7c808f0c8dcb50fa75ea7923115d5d235f0ee
trygvemm <trygvemyhr@gmail.com>
2023-08-05 22:39:10 +0200

    Finished

--- bot.py
+++ bot.py
@@ -14,21 +14,22 @@ def send_email():
 
         with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
             smtp.starttls()
-            smtp.login('trygvisbot@gmail.com', 'tyewxvltcqdsktzm')
+            smtp.login('trygvisbot@gmail.com', 'tyewxvltcqdsktzm') # Dont look here. Private password
             smtp.sendmail(fromaddr, toaddrs, msg.as_string())
             print('sent')
     
 while True:
+    pyautogui.click(86, 84)
+    time.sleep(60*5)
     screenshot = pyautogui.screenshot()
-    pixel_color = screenshot.getpixel((100,100))
+    pixel_color = screenshot.getpixel((438,545))
     
     if previous_color is not None and pixel_color != previous_color:
         print('SIT ledig')
-        send_email()
+        #send_email()
     
     previous_color = pixel_color
-    # Sleep for ten seconds
-    time.sleep(10)
+    
 
 
     
