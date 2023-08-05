63e60b39f53b2079a4a46ba675426fca1f47fc69
trygvemm <trygvemyhr@gmail.com>
2023-08-05 22:00:37 +0200

    shit works

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
-print("hello wordd")
+import  pyautogui
+import time
+
+previous_color = None
+    
+while True:
+    screenshot = pyautogui.screenshot()
+    pixel_color = screenshot.getpixel((100,100))
+    
+    if previous_color is not None and pixel_color != previous_color:
+        print("Pixel color changed!")
+    
+    previous_color = pixel_color
+    # Sleep for ten seconds
+    time.sleep(10)
